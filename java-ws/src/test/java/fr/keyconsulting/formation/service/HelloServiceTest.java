00dba2c43a4475603a5fa8c29d77bea7e8aeb59e
Meric Garcia <meric.garcia@keyconsulting.fr>
2015-10-12 14:31:56 +0200

    Add Junit and first test

---
+++ java-ws/src/test/java/fr/keyconsulting/formation/service/HelloServiceTest.java
@@ -0,0 +1,15 @@
+package fr.keyconsulting.formation.service;
+
+import org.junit.Test;
+
+
+public class HelloServiceTest {
+
+	private HelloService service = new HelloService();
+	
+	@Test
+	public void sayHello(){
+
+	}
+	
+}
